33faa8f4d08b7702402b8725126beea4fda42d54
Pascal <pascalbbcn@gmail.com>
2022-09-22 19:51:03 +0200

    add input for user to choose r/p/s

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,14 +1,25 @@
+function playRound(playerSelection, computerSelection) {
+    return playerSelection + computerSelection;
+    
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(prompt("rock, paper, or scissors?"), computerSelection));
+
+
 
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random()* 3) + 1;
     if (randomChoice === 1) {
-        return "Rock"
+        return "rock"
     } else if (randomChoice === 2) {
-        return "Paper"
+        return "paper"
     }
     else {
-        return "Scissors"
+        return "scissors"
     }
 }
 console.log(getComputerChoice());
+
 
