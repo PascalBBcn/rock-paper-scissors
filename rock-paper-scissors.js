9728dc762291011e09dff6c2ca289a4268872ca9
Pascal <pascalbbcn@gmail.com>
2022-09-22 18:20:12 +0200

    Create external link from Index.html to rock-paper-scissors.jsCreate randomizer for getComputerChoice

---
+++ rock-paper-scissors.js
@@ -0,0 +1,14 @@
+
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random()* 3) + 1;
+    if (randomChoice === 1) {
+        return "Rock"
+    } else if (randomChoice === 2) {
+        return "Paper"
+    }
+    else {
+        return "Scissors"
+    }
+}
+console.log(getComputerChoice());
+
