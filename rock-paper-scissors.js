4ef9b81528109e5f5be62772d2233a880e7fe5e5
Pascal <pascalbbcn@gmail.com>
2022-09-23 17:13:45 +0200

    Add5 rounds to the game. Add a scoring system.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,28 +1,57 @@
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
+
+
+function game() {
+    for (let roundNum = 0; roundNum < 5; roundNum++) {
+        let results = playRound(prompt("rock, paper, or scissors?").toLowerCase(), getComputerChoice());
+        console.log(roundNum);
+        console.log(results); 
+        }
+    if (playerScore > computerScore) {
+        console.log("You win!");
+    } else if (playerScore < computerScore) {
+        console.log("You lose!");
+    } else {
+        console.log("It's a tie.");
+    }
+}
+    console.log(game());
+
+
 function playRound(playerSelection, computerSelection) {
     let result;
     if (playerSelection === computerSelection) {
         result = "Tie"
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        result = "You lose! paper beats rock."
+        result = "You lose this round! paper beats rock."
+        computerScore += 1;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        result = "You win! rock beats scissors."
+        result = "You win this round! rock beats scissors."
+        playerScore += 1;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        result = "You win! paper beats rock."
+        result = "You win this round! paper beats rock."
+        playerScore += 1;
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        result = "You lose! scissors beats paper."
+        result = "You lose this round! scissors beats paper."
+        computerScore += 1;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        result = "You lose! rock beats scissors."
+        result = "You lose this round! rock beats scissors."
+        computerScore += 1;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        result = "You win! scissors beats paper."
+        result = "You win this round! scissors beats paper."
+        playerScore += 1;
+    
     }
 
-    return "You:" + playerSelection + " NPC:" + computerSelection + "  " + result;
+    return "You:" + playerSelection + " NPC:" + computerSelection + "  " + result + " PS: " + playerScore + " CS: " + computerScore;
+    
 
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(prompt("rock, paper, or scissors?"), computerSelection));
+//console.log(playRound(prompt("rock, paper, or scissors?").toLowerCase(), computerSelection));
 
 
 
@@ -37,6 +66,3 @@ function getComputerChoice() {
         return "scissors"
     }
 }
-
-
-
