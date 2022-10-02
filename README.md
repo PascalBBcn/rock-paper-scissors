2deca346e76fed28a4434745e11d8d0dd5006bb1
Pascal <pascalbbcn@gmail.com>
2022-10-02 15:30:37 +0200

    Add buttons to rock, paper, and scissors. Convert console.log() results to div on webpage

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rock-paper-scissors
 TOP rock paper scissors project
+
+Pseudocode:
+Define function "getComputerChoice"
+Use math.random to choose a random number
+if 1 show rock, 2 paper, 3 scissors
--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,58 +1,144 @@
-const playerSelection = "rock";
+
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// let playerScore = 0;
+// let computerScore = 0;
+
+
+// function game() {
+//     for (let roundNum = 0; roundNum < 5; roundNum++) {
+//         let results = playRound(prompt("rock, paper, or scissors?").toLowerCase(), getComputerChoice());
+//         console.log(roundNum);
+//         console.log(results); 
+//         }
+//     if (playerScore > computerScore) {
+//         console.log("You win!");
+//     } else if (playerScore < computerScore) {
+//         console.log("You lose!");
+//     } else {
+//         console.log("It's a tie.");
+//     }
+// }
+//     console.log(game());
+
+
+// function playRound(playerSelection, computerSelection) {
+//     let result;
+//     if (playerSelection === computerSelection) {
+//         result = "Tie"
+//     } else if (playerSelection === "rock" && computerSelection === "paper") {
+//         result = "You lose this round! paper beats rock."
+//         computerScore += 1;
+//     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+//         result = "You win this round! rock beats scissors."
+//         playerScore += 1;
+//     } else if (playerSelection === "paper" && computerSelection === "rock") {
+//         result = "You win this round! paper beats rock."
+//         playerScore += 1;
+//     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+//         result = "You lose this round! scissors beats paper."
+//         computerScore += 1;
+//     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+//         result = "You lose this round! rock beats scissors."
+//         computerScore += 1;
+//     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+//         result = "You win this round! scissors beats paper."
+//         playerScore += 1;
+    
+//     }
+
+//     return "You:" + playerSelection + " NPC:" + computerSelection + "  " + result + " PS: " + playerScore + " CS: " + computerScore;
+    
+
+// }
+
+// //console.log(playRound(prompt("rock, paper, or scissors?").toLowerCase(), computerSelection));
+
+
+
+// function getComputerChoice() {
+//     let randomChoice = Math.floor(Math.random()* 3) + 1;
+//     if (randomChoice === 1) {
+//         return "rock"
+//     } else if (randomChoice === 2) {
+//         return "paper"
+//     }
+//     else {
+//         return "scissors"
+//     }
+// }
+
+
+
+
+
+
+
+
+let playerSelection;
 const computerSelection = getComputerChoice();
 let playerScore = 0;
 let computerScore = 0;
 
+// function game() {
+//     for (let roundNum = 0; roundNum < 5; roundNum++) {
+//         let results = playRound(prompt("rock, paper, or scissors?").toLowerCase(), getComputerChoice());
+//         console.log(roundNum);
+//         console.log(results); 
+//         }
+//     if (playerScore > computerScore) {
+//         console.log("You win!");
+//     } else if (playerScore < computerScore) {
+//         console.log("You lose!");
+//     } else {
+//         console.log("It's a tie.");
+//     }
+// }
+//     console.log(game());
 
-function game() {
-    for (let roundNum = 0; roundNum < 5; roundNum++) {
-        let results = playRound(prompt("rock, paper, or scissors?").toLowerCase(), getComputerChoice());
-        console.log(roundNum);
-        console.log(results); 
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  
+    button.addEventListener('click', () => {
+        
+    function playRound(playerSelection = button.id, computerSelection) {
+
+        let result;
+        if (playerSelection === computerSelection) {
+            result = "Tie"
+        } else if (playerSelection === "rock" && computerSelection === "paper") {
+            result = "You lose this round! paper beats rock."
+            computerScore += 1;
+        } else if (playerSelection === "rock" && computerSelection === "scissors") {
+            result = "You win this round! rock beats scissors."
+            playerScore += 1;
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            result = "You win this round! paper beats rock."
+            playerScore += 1;
+        } else if (playerSelection === "paper" && computerSelection === "scissors") {
+            result = "You lose this round! scissors beats paper."
+            computerScore += 1;
+        } else if (playerSelection === "scissors" && computerSelection === "rock") {
+            result = "You lose this round! rock beats scissors."
+            computerScore += 1;
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            result = "You win this round! scissors beats paper."
+            playerScore += 1;
+        
         }
-    if (playerScore > computerScore) {
-        console.log("You win!");
-    } else if (playerScore < computerScore) {
-        console.log("You lose!");
-    } else {
-        console.log("It's a tie.");
+        const resultContainer = document.querySelector('#resultContainer');
+        resultContainer.textContent = "|You: " + playerSelection + " NPC: " + computerSelection + "|" + "  " + result + " PS: " + playerScore + " CS: " + computerScore;;
+
+        return "You: " + playerSelection + " NPC: " + computerSelection + "  " + result + " PS: " + playerScore + " CS: " + computerScore;
     }
-}
-    console.log(game());
-
-
-function playRound(playerSelection, computerSelection) {
-    let result;
-    if (playerSelection === computerSelection) {
-        result = "Tie"
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        result = "You lose this round! paper beats rock."
-        computerScore += 1;
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        result = "You win this round! rock beats scissors."
-        playerScore += 1;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        result = "You win this round! paper beats rock."
-        playerScore += 1;
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        result = "You lose this round! scissors beats paper."
-        computerScore += 1;
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        result = "You lose this round! rock beats scissors."
-        computerScore += 1;
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        result = "You win this round! scissors beats paper."
-        playerScore += 1;
     
-    }
-
-    return "You:" + playerSelection + " NPC:" + computerSelection + "  " + result + " PS: " + playerScore + " CS: " + computerScore;
-    
-
-}
+    console.log(playRound(playerSelection, computerSelection));
+    });
+});
 
 //console.log(playRound(prompt("rock, paper, or scissors?").toLowerCase(), computerSelection));
-
+ //console.log(playRound(playerSelection, computerSelection));
 
 
 function getComputerChoice() {
@@ -66,3 +152,31 @@ function getComputerChoice() {
         return "scissors"
     }
 }
+
+
+
+
+
+
+
+
+
+
+// const rockBtn = document.querySelector('#rockBtn');
+// rockBtn.addEventListener('click', () => {
+//     playerSelection === "rock";
+// });
+
+// const paperBtn = document.querySelector('#paperBtn');
+// paperBtn.addEventListener('click', () => {
+//     playerSelection === "paper";
+    
+// });
+
+// const scissorsBtn = document.querySelector('#scissorsBtn');
+// paperBtn.addEventListener('click', () => {
+//     playerSelection === "
+    
+// });
+
+
